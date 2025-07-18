37092d44b8ea6d17361badade84053fe36ab4196
dell <dell@DESKTOP-MMECUAS>
2025-07-18 07:08:53 +0530

    Pushing the project code into the github

---
+++ mmp/src/main/java/org/iitwf/hc/mmp/pages/LoginPage.java
@@ -0,0 +1,26 @@
+package org.iitwf.hc.mmp.pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+public class LoginPage {
+	
+	  protected WebDriver driver;
+	  private By usernameBy = By.id("username");
+	  private By passwordBy = By.id("password");
+	  private By signinBy = By.xpath("//input[@value='Sign In']");
+
+	  public LoginPage(WebDriver driver){
+	    this.driver = driver;
+	     if (!driver.getTitle().equals("Login")) {
+	      throw new IllegalStateException("This is Login Page," +
+	            " current page is: " + driver.getCurrentUrl());
+	    }
+	  }
+	  public HomePage loginValidUser(String userName, String password) {
+	    driver.findElement(usernameBy).sendKeys(userName);
+	    driver.findElement(passwordBy).sendKeys(password);
+	    driver.findElement(signinBy).click();
+	    return new HomePage(driver);
+	  }
+
+}
+  
---
+++ mmp/src/main/java/org/iitwf/hc/mmp/pages/MessagesAdminPage.java
@@ -0,0 +1,33 @@
+package org.iitwf.hc.mmp.pages;
+
+import java.util.HashMap;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class MessagesAdminPage {
+	protected WebDriver driver;
+	/**
+	 * Constructor for MessagesPage
+	 * @param driver WebDriver instance to interact with the browser
+	 */
+
+	public MessagesAdminPage(WebDriver driver){
+		this.driver = driver;
+		if (!driver.getTitle().equals("Messages")) {
+			throw new IllegalStateException("This is not Messages Page," +
+					" current page is: " + driver.getCurrentUrl());
+		}
+	}
+	public HashMap<String, String> getMessageDetails(String subject) {
+		HashMap<String, String> actualHMap = new HashMap<String, String>();
+		// Logic to retrieve message details goes here
+		System.out.println("Retrieving message details for subject: " + subject);
+		actualHMap.put("subject", subject);
+		actualHMap.put("message", driver.findElement(By.xpath("//b[text()='"+subject+"']/parent::td/preceding-sibling::td/b")).getText());
+		actualHMap.put("date", driver.findElement(By.xpath("//b[text()='"+subject+"']/parent::td/following-sibling::td/b")).getText());
+		actualHMap.put("patientName", driver.findElement(By.xpath("//b[text()='"+subject+"']/ancestor::tr/following-sibling::tr[1]/td[2]")).getText());
+		return actualHMap;
+	}
+
+}
